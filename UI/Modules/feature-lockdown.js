ab844256fb978ff2179fd77b33207029ebebe72f
ΣΝΛ <78288792+4zx16@users.noreply.github.com>
2026-02-01 00:29:25 -0800

    Add SPDX license header to feature-lockdown.js
    Add license header to feature-lockdown.js
    
    Signed-off-by: ΣΝΛ <78288792+4zx16@users.noreply.github.com>

--- UI/Modules/feature-lockdown.js
+++ UI/Modules/feature-lockdown.js
@@ -1,3 +1,7 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright (C) 2026 Krynet, LLC
+ * https://github.com/Bloodware-Inc/KrySearch
+ */
 (function () {
   const plugin = {
     id: "feature-lockdown-max",
